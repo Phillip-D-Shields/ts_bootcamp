9615e2f70f0ddb10f3faa908129a47c65c02ef23
phillip shields <matua.phillip.shields@gmail.com>
2022-08-29 21:35:43 +1200

    finished config section, updated structure

--- src/basics-00.ts
+++ src/basics-00.ts
@@ -1,6 +1,6 @@
 // number, string, boolean
 
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+function add00(n1: number, n2: number, showResult: boolean, phrase: string) {
   if (showResult) console.log(`${phrase} ${n1 + n2}`);
   else return n1 + n2;
 }
@@ -9,7 +9,7 @@ let number0: number;
 number0 = 100;
 const number1 = 5;
 const number2 = 4.5;
-const printResult = true;
+const showResult = true;
 const resultPhrase = "result is: ";
 
 // const result = add(number1, number2, printResult);
@@ -17,4 +17,4 @@ const resultPhrase = "result is: ";
 // console.log(result);
 // console.log(typeof result);
 
-add(number1, number2, printResult, resultPhrase);
+add00(number1, number2, showResult, resultPhrase);
